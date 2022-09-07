062838543967b7f346dd8d79942b6b4a456adffb
evreekum <e.v.reekum@gmail.com>
2022-09-07 13:54:40 +0200

    continued the assignment

--- src/App.js
+++ src/App.js
@@ -2,56 +2,76 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [nameValue, setNameValue] = React.useState('');
+    const [ageValue, setAgeValue] = React.useState(0);
+    const [messageValue, setMessageValue] = React.useState('');
+    const [newsLetterValue, setNewsLetterValue] = React.useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
+
     return (
-        <div className="form-container">
 
-            <form>
-                <div className="person-data">
-                    <h2>gegevens</h2>
-                    <label htmlFor="name-field">
-                        naam:
-                        <input
-                            type="text"
-                            id="name-field"
+        <form onSubmit={handleSubmit}>
+            <h2>gegevens</h2>
+            <label htmlFor="name-field">
+                naam:
+                <input
+                    type="text"
+                    name="name"
+                    id="name-field"
+                    value={nameValue}
+                />
+            </label>
 
-                        />
-                    </label>
+            <label htmlFor="age-field">
+                leeftijd:
+                <input
+                    type="number"
+                    name="age"
+                    id="age-field"
+                    value={ageValue}
 
-                    <label htmlFor="age-field">
-                        leeftijd:
-                        <input
-                            type="number"
-                            id="age-field"
-                            placeholder="0"
-                        />
-                    </label>
-                </div>
-                <div className="person-review">
-                    <h2>jou review</h2>
-                    <label htmlFor="comments-field">
-                        opmerkingen:
-                        <input
-                            type="text"
-                            id="comments-field"
-                            placeholder="Wat vond je van het recept?"
-                        />
-                    </label>
+                />
+            </label>
 
-                    <label htmlFor="newsletter-box">
-                        <input
-                            type="checkbox"
-                            id="newsletter-box"
-                        />
-                        Ik schrijf me in voor de nieuwsbrief
-                    </label>
-                </div>
-            </form>
+
+            <h2>jouw review</h2>
+            <label htmlFor="comments-field">
+                opmerkingen:
+                <textarea
+                    name="message"
+
+                    id="comments-field"
+                    cols="45"
+                    rows="5"
+                    placeholder="Wat vond je van het recept?"
+                    value={messageValue}
+                >
+
+                </textarea>
+
+            </label>
+
+            <label htmlFor="newsletter-box">
+                <input
+                    type="checkbox"
+                    name="newsletter"
+                    checked={newsLetterValue}
+                    id="newsletter-box"
+                />
+                Ik schrijf me in voor de nieuwsbrief
+            </label>
             <button
                 type="submit"
             >
                 Versturen
             </button>
-        </div>
+        </form>
+
     );
 }
 
